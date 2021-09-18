295c2118155a0eb442312b4a1b26cd3439a539d7
bilakispa <bilakispa@gmail.com>
2021-09-18 20:13:52 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">Tetredux</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
